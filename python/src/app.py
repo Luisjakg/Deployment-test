3c411bec8d104173b7dc423faaee8aaf943cca01
Luisjakg <52958375+Luisjakg@users.noreply.github.com>
2023-06-06 00:05:50 -0500

    awdawdw

--- python/src/app.py
+++ python/src/app.py
@@ -1,8 +1,4 @@
-import os
-import json 
-
 def lambda_handler(event, context):
-    json_region = os.environ['AWS_REGION']
     return "Luis Javier Karam"  # Return a string with my name :D
 
 # This is the main function, it will be executed when the lambda is called
