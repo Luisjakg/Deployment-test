710bbef4a826f064664a7279e4411c0d94facc8b
Luisjakg <52958375+Luisjakg@users.noreply.github.com>
2023-06-05 19:48:32 -0500

    Fixed lint problems

--- python/src/app.py
+++ python/src/app.py
@@ -1,10 +1,9 @@
 import os
-import json
 
 def lambda_handler(event, context):
     json_region = os.environ['AWS_REGION']
-    json_account_id = os.environ['AWS_ACCOUNT_ID'] # AWS_ACCOUNT_ID
-    json_function_name = os.environ['AWS_LAMBDA_FUNCTION_NAME'] # AWS_LAMBDA_FUNCTION_NAME
-    return "Luis Javier Karam" # Return a string with my name:D
+    json_account_id = os.environ['AWS_ACCOUNT_ID']  # AWS_ACCOUNT_ID
+    json_function_name = os.environ['AWS_LAMBDA_FUNCTION_NAME']  # AWS_LAMBDA_FUNCTION_NAME
+    return "Luis Javier Karam"  # Return a string with my name :D
 
 # This is the main function, it will be executed when the lambda is called
